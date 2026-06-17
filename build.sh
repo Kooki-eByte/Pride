ba3b6e70410b30b7a60f72bc0a380d8f7e71e001
Cristian Hornedo <cristianhornedo@gmail.com>
2026-06-17 14:52:15 -0400

    Add in quick boilerplate and new usage for pride

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+CC=gcc
+
+$CC pride.c -o ./build/test
+
+./build/test
---
+++ rex_defines.h
@@ -0,0 +1,32 @@
+#ifndef REX_DEFINES_H
+#define REX_DEFINES_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef i8 b8;
+typedef i32 b32;
+
+#define KiB64(n) ((u64)(n) << 10)
+#define MiB64(n) ((u64)(n) << 20)
+#define GiB64(n) ((u64)(n) << 30)
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+#define ALIGN_POW2(n, p) (((u64)(n) + ((u64)(p) - 1)) & (~((u64)(p) - 1)))
+
+#define global static
+#define internal static
+#define local_persist static
+
+#endif // REX_DEFINES_H
